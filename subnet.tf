75c95cb5c5ed5fccc33e0b77a8f438531f81dbeb
JoaoPresto <jguedespresto@gmail.com>
2022-11-07 18:29:10 -0300

    Criar recurso network security group

--- subnet.tf
+++ subnet.tf
@@ -11,3 +11,14 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/22"]
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.subnet_name
+  location            = azurerm_resource_group.gitops_rg.location
+  resource_group_name = azurerm_resource_group.gitops_rg.name  
+}
+
+resource "azurerm_subnet_network_security_group_association" "association" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
