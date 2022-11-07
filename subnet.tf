8f0a48fbe55e6de9ff772ee6581bc635706575d6
JoaoPresto <jguedespresto@gmail.com>
2022-11-07 18:52:26 -0300

    Arrumar formatação

--- subnet.tf
+++ subnet.tf
@@ -15,7 +15,7 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_network_security_group" "nsg" {
   name                = var.subnet_name
   location            = azurerm_resource_group.gitops_rg.location
-  resource_group_name = azurerm_resource_group.gitops_rg.name  
+  resource_group_name = azurerm_resource_group.gitops_rg.name
 }
 
 resource "azurerm_subnet_network_security_group_association" "association" {
