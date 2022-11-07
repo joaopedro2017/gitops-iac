a884c3ae457df40febb86d651070fb9ef3867c04
JoaoPresto <jguedespresto@gmail.com>
2022-11-07 18:14:40 -0300

    Adciona grupo de recursos

--- main.tf
+++ main.tf
@@ -17,3 +17,8 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "gitops_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "resource_group_name" {
+  default = "eastus2-gitops-terraform-rg"
+}
+
+variable "location" {
+  default = "eastus2"
+}
