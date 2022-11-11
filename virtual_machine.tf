ab8694dae70ce98feec8413e8c91386451c4bb4a
JoaoPresto <jguedespresto@gmail.com>
2022-11-10 23:45:38 -0300

    Criação de novos arquivos

--- virtual_machine.tf
+++ virtual_machine.tf
@@ -1,15 +1,3 @@
-resource "azurerm_network_interface" "nic" {
-  name                = "gitops-nic"
-  location            = azurerm_resource_group.gitops_rg.location
-  resource_group_name = azurerm_resource_group.gitops_rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_windows_virtual_machine" "virtual_machine" {
   name                = var.virtual_machine_name
   computer_name       = var.virtual_machine_name
