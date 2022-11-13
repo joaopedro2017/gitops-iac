aba8cc7042b43c69e683b87c8f21d294a89493f9
JoaoPresto <jguedespresto@gmail.com>
2022-11-13 15:49:56 -0300

    Renomear nsg

--- vnet.tf
+++ vnet.tf
@@ -13,7 +13,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "production-nic"
+  name                = "production-nsg"
   location            = azurerm_resource_group.gitops_rg.location
   resource_group_name = azurerm_resource_group.gitops_rg.name
 }
